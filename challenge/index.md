f46b3803087274e53fe43f40cd1c13eb4e958b45
Nick Jones <nickjones9605@gmail.com>
2022-03-31 20:23:52 -0600

    Initial commit, Nick J and Mary T mod 0 session 5 partner excersise

---
+++ challenge/index.md
@@ -0,0 +1,11 @@
+#Book titles
+Of Mice and Men
+The Great Gatsby
+The English Patient
+Love in the Time of Cholera
+Catcher in the Rye
+
+
+Mary T navigated, and I (Nick J) drove for the most part. We did both drive for a bit.
+We slowed down on the header requirement, and we had to look at the markdown cheatsheet.
+We didn't disagree on anything.
---
+++ challenge/ruby_partner_collab.rb
@@ -0,0 +1,18 @@
+#I named it ruby partner collab because it's in ruby and we're collabing with a partner for this session!
+
+
+#declare integers for page count
+page_count = 500
+
+#declare a float type variable for price
+average_price = 9.99
+
+#declare a boolean type variable for if a book is on sale, or if it's been read before 
+on_sale = true
+is_read = true
+is_first_edition = true
+author = "John Steinbeck"
+publishing_company = "Penguin"
+
+#Declare array of random page counts
+book_page_count = [ 500, 250, 30, 1000]
